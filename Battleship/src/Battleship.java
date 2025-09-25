c7ef72873340fbca10af1d6c6892ba1ad041a77a
caden <caden@10.0.0.123>
2025-09-24 21:07:21 -0600

    Added instructions method, hard coded the first few line of playGame(), assured myself of Github operation

--- Battleship/src/Battleship.java
+++ Battleship/src/Battleship.java
@@ -1,17 +1,44 @@
+import java.util.Scanner;
 
 public class Battleship
 	{
-
+		static Scanner userIntInput = new Scanner(System.in);
+		static Scanner userInput = new Scanner(System.in);
+		
 		public static void main(String[] args)
 			{
-			introduction();
+			// THIS IS MY MESSAGE TO MR. MCGUIRE THAT I HAVE IN FACT SUCCESSFULLY OPERATED GITHUB
 			Board.displayBoard();
 			Board.generateEnemy();
+			introduction();
 			}
 		
 		public static void introduction()
 		{
 			System.out.println("Welcome to Battleship. Do you know how to play? (1) Yes (2) No");
+			int instructions = userIntInput.nextInt();
+			if (instructions == 2)
+			{
+				System.out.println(" ");
+				System.out.println("There are two enemy ships at unknown locations on a 5x5 square board like the one before you.");
+				System.out.println("One can be sunk in two shots, the other in three.");
+				System.out.println("You will select a given location with a row and column identifier (like A1).");
+				System.out.println("Hits and misses will be tracked on the board in front of you.");
+				System.out.println("You must land the exact number of hits in the exact location to sink a ship.");
+				System.out.println("Good luck!");
+				System.out.println("Now...");
+				PlayGame.playGame();
+			}
+			else if (instructions == 1)
+			{
+				PlayGame.playGame();
+			}
+			else
+			{
+				System.out.println("I'll take that as a no.");
+				PlayGame.playGame();
+			}
+			
 		}
 
 	}
--- Battleship/src/PlayGame.java
+++ Battleship/src/PlayGame.java
@@ -1,8 +1,20 @@
 
 public class PlayGame
-	{
+	{	
+	// We'll want a while loop and a boolean to loop until complete
 		public static void playGame()
 			{
-				// Here is where the main code will go.
+				System.out.println("Where would you like to aim?");
+				String target = Battleship.userInput.nextLine();
+				switch (target)
+				{
+				case "C3":
+					System.out.println("It's a hit!");
+					break;
+				case "C4":
+					System.out.println("It's a hit!");
+					break;
+				}
+			
 			}
 	}
